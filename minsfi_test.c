13654b4d361c09f7be80bfe99c753ca9ab83ca2e
Mark Seaborn <mseaborn@chromium.org>
2014-05-26 16:32:12 +0000

    Test direct and indirect calls with arguments

--- minsfi_test.c
+++ minsfi_test.c
@@ -6,13 +6,21 @@ void func1(void) {
   write(1, str, sizeof(str) - 1);
 }
 
+void func2(int fd) {
+  static const char str[] = "In second address-taken function\n";
+  write(fd, str, sizeof(str) - 1);
+}
+
 void (*func1_ptr)(void) = func1;
+void (*func2_ptr)(int fd) = func2;
 
 int main() {
   static const char str[] = "Hello, world!\n";
   write(1, str, sizeof(str) - 1);
 
   func1_ptr();
+  func2_ptr(1);
+  func2(1);
 
   return 0;
 }
